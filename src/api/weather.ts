073eb8e8b9921ed21b9bd66506e7a5c18e5e1b5c
mavka <VladislavaDesu@gmail.com>
2025-01-02 22:50:17 +0100

    use correct key

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -5,19 +5,10 @@ export function fetchWeatherData({
   lat,
   lon,
 }: Coordinates): Promise<WeatherData> {
-  return fetch(`/api/proxy?lat=${lat}&lon=${lon}&endpoint=weather`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    }
-  );
-}
+  const query = `${lat},${lon}`;
 
-export function fetchCityLocation(query: string): Promise<GeoNameResponse> {
   return fetch(
-    `/api/proxy?query=${encodeURIComponent(query)}&endpoint=city`
+    `http://api.weatherapi.com/v1/forecast.json?key=5f28edb39ec341cd94b202908250201&q=${query}&days=10&aqi=no&alerts=no`
   ).then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -26,16 +17,26 @@ export function fetchCityLocation(query: string): Promise<GeoNameResponse> {
   });
 }
 
+export function fetchCityLocation(query: string): Promise<GeoNameResponse> {
+  return fetch(
+    `http://api.geonames.org/searchJSON?q=${query}&orderby=relevance&username=vladyslavayakovenko`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  });
+}
 export function fetchCityLocationByCoords({
   lat,
   lon,
 }: Coordinates): Promise<GeoNameResponse> {
-  return fetch(`/api/proxy?lat=${lat}&lon=${lon}&endpoint=coords`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
+  return fetch(
+    `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lon}&fclass=P&fcode=PPLA&fcode=PPL&fcode=PPLC&username=vladyslavayakovenko`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  );
+    return response.json();
+  });
 }
